ed9cb8fb20890d3d6e7d9a60f2587d4cb500054c
thomask77 <mail+git@t-kindler.de>
2012-07-17 14:53:34 +0300

    Simple battery voltage check

--- MotorMate.c
+++ MotorMate.c
@@ -17,40 +17,48 @@
 #include <avr/wdt.h>
 #include <util/delay.h>
 
-// Reverse-enginered pinout
+// Reverse-engineered pinout
 //
 #define LS_A    _BV(PD0)
 #define LS_B    _BV(PD1)
-#define RC_IN   _BV(PD2)
+#define RC_IN   _BV(PD2)    // INT0
 #define LS_C    _BV(PD3)
 #define HS_C    _BV(PD4)
 #define HS_A    _BV(PD5)
-#define U_NULL  _BV(PD6)
+#define U_NULL  _BV(PD6)    // AIN0
 #define HS_B    _BV(PD7)
-#define U_A     _BV(PC2)
-#define U_B     _BV(PC3)
-#define U_C     _BV(PC4)
-// #define ???     PC5
-#define U_BAT   ADC7
+#define U_A     _BV(PC2)    // ADC2
+#define U_B     _BV(PC3)    // ADC3
+#define U_C     _BV(PC4)    // ADC4
 
-#define PWM_PERIOD    117     // ca. 8kHz
-#define PWM_MIN       8
-#define PWM_MAX       (PWM_PERIOD-8)
+// Software PWM constants
+//
+#define PWM_PERIOD  117     // ca. 8kHz
+#define PWM_MIN     8
+#define PWM_MAX     (PWM_PERIOD-8)
+
+// ADC conversion constants
+//
+#define ADC_PER_VOLT    ((1024 / 2.56) / ((10e3 + 390) / 390))
+
+#define NUM_CELLS   4
+#define U_BAT_MIN   (3.0 * NUM_CELLS * ADC_PER_VOLT)
 
 uint8_t  pwm_state;
 uint8_t  pwm_t0, pwm_out0;
 uint8_t  pwm_t1, pwm_out1;
 uint8_t  pwm_dead;
+
 int16_t  rc_pulse;
 int8_t   rc_rxcount;
 
-inline void deadtime_1us(void)
+static inline void deadtime_1us(void)
 {
     PORTD = pwm_dead; PORTD = pwm_dead; PORTD = pwm_dead; PORTD = pwm_dead;
     PORTD = pwm_dead; PORTD = pwm_dead; PORTD = pwm_dead; PORTD = pwm_dead;
 }
 
-inline void deadtime_8us(void)
+static inline void deadtime_8us(void)
 {
     deadtime_1us(); deadtime_1us(); deadtime_1us(); deadtime_1us();
     deadtime_1us(); deadtime_1us(); deadtime_1us(); deadtime_1us();
@@ -109,7 +117,6 @@ ISR(INT0_vect)
 {
     if (PIND & RC_IN) {
         TCNT1 = 0;
-        return;
     }
     else {
         rc_pulse = TCNT1;
@@ -131,34 +138,38 @@ int main(void)
 
     // Use timer 0 for software PWM output
     //
-    TCCR0  = 2;
-    TIMSK |= _BV(TOV0);
+    TCCR0  = 2;             // 1us timebase
+    TIMSK |= _BV(TOV0);     // interrupt on overflow
     set_pwm(0);
 
     // Use timer 1 and INT0 for RC input
     //
-    TCCR1B = 2;           // 1us timebase
-    MCUCR = _BV(ISC00);   // trigger on any change
-    GICR  = _BV(INT0);    // enable interrupt
+    TCCR1B = 2;             // 1us timebase
+    MCUCR = _BV(ISC00);     // trigger on any change
+    GICR  = _BV(INT0);      // enable interrupt
+
+    // Use ADC7 for battery voltage measurement
+    // 125kHz ADC Clock / 13 => ~4800 samples/s
+    // Use internal 2.56V reference (TODO: Check this!)
+    //
+    ADMUX  = _BV(REFS1) | _BV(REFS0) | 7;
+    ADCSRA = _BV(ADEN) | _BV(ADSC) | _BV(ADFR) | 7;
 
     sei();
-
-    _delay_ms(500);
+    
+    // Wait for RC receiver to start up, etc..
+    //
+    _delay_ms(250);
 
     for(;;) {
-        // TODO:
-        // * Check battery voltage
-        // * Check for RC timeouts
-        // * Limit PWM slew rate
-        // * Arm/Disarm motors, beep on startup
-        // * Can we estimate the motor current?!
-        // * Make use of the third output channel
-        //
         cli(); int t = rc_pulse; sei();
+        int u_bat = ADC;
 
-        if (t > 500 && t < 2500) {
-            int pwm = ((t - 1500) / 500.0) * PWM_MAX;
-            set_pwm(pwm);
+        if (u_bat < U_BAT_MIN) {
+            set_pwm(0);
+        }
+        else if (t > 500 && t < 2500) {
+            set_pwm(((t - 1500) / 500.0) * PWM_MAX);
         }
 
         _delay_ms(10);
